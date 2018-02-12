d9863b053b98b38c031a29a563a6a4c1891f628c
Wietse Wind <mail@wietse.com>
2018-02-12 02:25:38 +0100

    Add tip origin context and limit results (performance)

--- php/_activityfeed.php
+++ php/_activityfeed.php
@@ -5,48 +5,61 @@ if(!empty($o_postdata) && is_object($o_postdata)){
         $query = $db->prepare("
             SELECT
                 G1.*,
-                CONCAT(SUBSTRING(UPPER(type),1,1),'#',id) as id,
+                CONCAT(SUBSTRING(UPPER(G1.type),1,1),'#',G1.id) as id,
                 ufrom.userid as user_id,
-                uto.userid as to_id
+                uto.userid as to_id,
+                IF(G1.context IS NULL, message.context, G1.context) as context
             FROM
             (
 
-                SELECT
+                (SELECT
                     tip.`id`,
                     tip.`moment`,
                     tip.`from_user` as `user`,
                     tip.`to_user` as `to`,
                     tip.`amount`,
                     'tip' as `type`,
-                    tip.`network`
+                    tip.`network`,
+                    tip.`context`,
+                    tip.`message`
                 FROM `tip`
+                ORDER BY `id` DESC
+                LIMIT 200)
                 -- WHERE tip.`from_user` != 'pepperew'
 
                 UNION
 
-                SELECT
+                (SELECT
                     deposit.`id`,
                     deposit.`moment`,
                     deposit.`user` as `user`,
                     null as `to`,
                     deposit.`amount`,
                     'deposit' as `type`,
-                    deposit.`network`
+                    deposit.`network`,
+                    '' as context,
+                    null as message
                 FROM `deposit`
+                ORDER BY `id` DESC
+                LIMIT 200)
                 -- WHERE `user` != 'pepperew'
 
                 UNION
 
-                SELECT
+                (SELECT
                     withdraw.`id`,
                     withdraw.`moment`,
                     withdraw.`user` as `user`,
                     null as `to`,
                     withdraw.`amount`,
                     'withdraw' as `type`,
-                    withdraw.`network`
+                    withdraw.`network`,
+                    '' as context,
+                    null as message
                 FROM `withdraw`
                 WHERE `amount` != 0
+                ORDER BY `id` DESC
+                LIMIT 200)
                 -- WHERE `user` != 'pepperew'
 
             ) G1
@@ -54,7 +67,10 @@ if(!empty($o_postdata) && is_object($o_postdata)){
                 `user` ufrom ON (ufrom.`username` = G1.`user`)
             LEFT JOIN
                 `user` uto ON (uto.`username` = G1.`to`)
+            LEFT JOIN
+                `message` ON (message.id = G1.message)
             ORDER BY moment DESC
+            LIMIT 200
         ");
         $query->execute();
         $json['feed'] = $query->fetchAll(PDO::FETCH_ASSOC);
