9f687303cc7a59e7f5f5fc20c9c7473bf5f2744d
Wietse Wind <mail@wietse.com>
2018-03-23 06:49:08 +0100

    Fix delivered amount

--- nodejs/storetransactions/app.js
+++ nodejs/storetransactions/app.js
@@ -31,14 +31,14 @@ var _storeTransaction = function (tx, fullTx) {
   if (tx.ledger_index <= closedLedger) {
     var destinationTag = parseInt(tx.DestinationTag||0)
     var transferAmount = (parseFloat(typeof tx.Amount !== 'undefined' ? tx.Amount : 0)/1000/1000)
-
+    if (typeof fullTx === 'undefined') fullTx = tx
     if (typeof fullTx.meta !== 'undefined' && typeof fullTx.meta.delivered_amount !== 'undefined') {
       // Another currency, unsupported @ tipbot
-      transferAmount = 0
+      transferAmount = (parseFloat(fullTx.meta.delivered_amount)/1000/1000)
     }
     if (typeof fullTx.meta !== 'undefined' && typeof fullTx.meta.DeliveredAamount !== 'undefined') {
       // Another currency, unsupported @ tipbot
-      transferAmount = 0
+      transferAmount = (parseFloat(fullTx.meta.DeliveredAamount)/1000/1000)
     }
 
     var consolePostFix = ' [NO DESTINATION, NON PAYMENT? ESCROW?]'
