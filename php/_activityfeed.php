4902684e2347afe533a0a5497939b146b968eaf6
Wietse Wind <mail@wietse.com>
2018-09-18 10:38:45 +0200

    Split ILP stats, add ILP sum

--- php/_activityfeed.php
+++ php/_activityfeed.php
@@ -17,94 +17,114 @@ if(!empty($o_postdata) && is_object($o_postdata)){
     if ($skip == 0) {
         $subqLimit = ' LIMIT ' . $limit;
     }
+
     try {
-        $query = $db->prepare("
-            SELECT
-                G1.*,
-                CONCAT(SUBSTRING(UPPER(G1.type),1,1),'#',G1.id) as id,
-                ufrom.userid as user_id,
-                uto.userid as to_id,
-                IF(G1.context IS NULL, message.context, G1.context) as context
-            FROM
-            (
-                (SELECT
-                    MAX(ilp_deposits.`id`) as id,
-                    MAX(ilp_deposits.`moment`) as moment,
-                    ilp_deposits.`user` as `user`,
-                    null as `to`,
-                    null as `user_network`,
-                    null as `to_network`,
-                    (SUM(ilp_deposits.`drops`) - SUM(ilp_deposits.`fee`)) / 1000000 as amount,
-                    'ILP deposit' as `type`,
-                    ilp_deposits.`network`,
-                    '' as context,
-                    null as message
-                FROM `ilp_deposits`
-                GROUP BY CONCAT(ilp_deposits.user, DATE_FORMAT(`moment`, '%Y%m%d%H'))
-                ORDER BY `moment` DESC, `amount` DESC $subqLimit)
+        if (!empty($_GET["ilp"])) {
+            $query = $db->prepare("
+                SELECT
+                    G1.*,
+                    CONCAT(SUBSTRING(UPPER(G1.type),1,1),'#',G1.id) as id,
+                    ufrom.userid as user_id,
+                    uto.userid as to_id,
+                    IF(G1.context IS NULL, message.context, G1.context) as context
+                FROM
+                (
+                    (SELECT
+                        ilp_deposits.`id` as id,
+                        ilp_deposits.`moment` as moment,
+                        ilp_deposits.`user` as `user`,
+                        null as `to`,
+                        null as `user_network`,
+                        null as `to_network`,
+                        ilp_deposits.`drops` - ilp_deposits.`fee` / 1000000 as amount,
+                        'ILP deposit' as `type`,
+                        ilp_deposits.`network`,
+                        '' as context,
+                        null as message
+                    FROM `ilp_deposits`
+                    ORDER BY `moment` DESC, `amount` DESC $subqLimit)
 
-                UNION
+                ) G1
+                LEFT JOIN
+                    `user` ufrom ON ( (ufrom.`username` = G1.`user` AND ufrom.`network` = G1.`network`) OR (ufrom.`username` = G1.`user` AND ufrom.`network` = G1.`user_network`) )
+                LEFT JOIN
+                    `user` uto ON ( (uto.`username` = G1.`to` AND uto.`network` = G1.`network`) OR (uto.`username` = G1.`to` AND uto.`network` = G1.`to_network`) )
+                LEFT JOIN
+                    `message` ON (message.id = G1.message)
+                ORDER BY moment DESC
+                LIMIT $skip, $limit
+            ");
+        } else {
+            $query = $db->prepare("
+                SELECT
+                    G1.*,
+                    CONCAT(SUBSTRING(UPPER(G1.type),1,1),'#',G1.id) as id,
+                    ufrom.userid as user_id,
+                    uto.userid as to_id,
+                    IF(G1.context IS NULL, message.context, G1.context) as context
+                FROM
+                (
+                    (SELECT
+                        tip.`id`,
+                        tip.`moment`,
+                        tip.`from_user` as `user`,
+                        tip.`to_user` as `to`,
+                        tip.`from_network` as `user_network`,
+                        tip.`to_network` as `to_network`,
+                        tip.`amount`,
+                        'tip' as `type`,
+                        tip.`network`,
+                        tip.`context`,
+                        tip.`message`
+                    FROM `tip`
+                    ORDER BY `id` DESC $subqLimit)
 
-                (SELECT
-                    tip.`id`,
-                    tip.`moment`,
-                    tip.`from_user` as `user`,
-                    tip.`to_user` as `to`,
-                    tip.`from_network` as `user_network`,
-                    tip.`to_network` as `to_network`,
-                    tip.`amount`,
-                    'tip' as `type`,
-                    tip.`network`,
-                    tip.`context`,
-                    tip.`message`
-                FROM `tip`
-                ORDER BY `id` DESC $subqLimit)
+                    UNION
 
-                UNION
+                    (SELECT
+                        deposit.`id`,
+                        deposit.`moment`,
+                        deposit.`user` as `user`,
+                        null as `to`,
+                        null as `user_network`,
+                        null as `to_network`,
+                        deposit.`amount`,
+                        'deposit' as `type`,
+                        deposit.`network`,
+                        '' as context,
+                        null as message
+                    FROM `deposit`
+                    ORDER BY `id` DESC $subqLimit)
 
-                (SELECT
-                    deposit.`id`,
-                    deposit.`moment`,
-                    deposit.`user` as `user`,
-                    null as `to`,
-                    null as `user_network`,
-                    null as `to_network`,
-                    deposit.`amount`,
-                    'deposit' as `type`,
-                    deposit.`network`,
-                    '' as context,
-                    null as message
-                FROM `deposit`
-                ORDER BY `id` DESC $subqLimit)
+                    UNION
 
-                UNION
+                    (SELECT
+                        withdraw.`id`,
+                        withdraw.`moment`,
+                        withdraw.`user` as `user`,
+                        null as `to`,
+                        null as `user_network`,
+                        null as `to_network`,
+                        withdraw.`amount`,
+                        'withdraw' as `type`,
+                        withdraw.`network`,
+                        '' as context,
+                        null as message
+                    FROM `withdraw`
+                    WHERE `amount` != 0
+                    ORDER BY `id` DESC $subqLimit)
 
-                (SELECT
-                    withdraw.`id`,
-                    withdraw.`moment`,
-                    withdraw.`user` as `user`,
-                    null as `to`,
-                    null as `user_network`,
-                    null as `to_network`,
-                    withdraw.`amount`,
-                    'withdraw' as `type`,
-                    withdraw.`network`,
-                    '' as context,
-                    null as message
-                FROM `withdraw`
-                WHERE `amount` != 0
-                ORDER BY `id` DESC $subqLimit)
-
-            ) G1
-            LEFT JOIN
-                `user` ufrom ON ( (ufrom.`username` = G1.`user` AND ufrom.`network` = G1.`network`) OR (ufrom.`username` = G1.`user` AND ufrom.`network` = G1.`user_network`) )
-            LEFT JOIN
-                `user` uto ON ( (uto.`username` = G1.`to` AND uto.`network` = G1.`network`) OR (uto.`username` = G1.`to` AND uto.`network` = G1.`to_network`) )
-            LEFT JOIN
-                `message` ON (message.id = G1.message)
-            ORDER BY moment DESC
-            LIMIT $skip, $limit
-        ");
+                ) G1
+                LEFT JOIN
+                    `user` ufrom ON ( (ufrom.`username` = G1.`user` AND ufrom.`network` = G1.`network`) OR (ufrom.`username` = G1.`user` AND ufrom.`network` = G1.`user_network`) )
+                LEFT JOIN
+                    `user` uto ON ( (uto.`username` = G1.`to` AND uto.`network` = G1.`network`) OR (uto.`username` = G1.`to` AND uto.`network` = G1.`to_network`) )
+                LEFT JOIN
+                    `message` ON (message.id = G1.message)
+                ORDER BY moment DESC
+                LIMIT $skip, $limit
+            ");
+        }
         $query->execute();
         $json['feed'] = $query->fetchAll(PDO::FETCH_ASSOC);
 
@@ -114,13 +134,15 @@ if(!empty($o_postdata) && is_object($o_postdata)){
                 `fee`,
                 `balance` - `fee` AS `ledgerbalance`,
                 `count`,
-                `sum`
+                `sum`,
+                `ilpsum`
             FROM (
                 SELECT
                     (SELECT sum(balance) as `balance` FROM `user`) as `balance`,
                     (SELECT sum(`fee`)/1000/1000 as `fee` FROM `withdraw`) as `fee`,
                     (SELECT count(1) as `count` FROM `tip`) as `count`,
-                    (SELECT sum(`amount`) as `sum` FROM `tip`) as `sum`
+                    (SELECT sum(`amount`) as `sum` FROM `tip`) as `sum`,
+                    (SELECT sum(`drops`) / 1000000 as `ilpsum` FROM `ilp_deposits`) as `ilpsum`
             ) G2
         ");
         $query->execute();
